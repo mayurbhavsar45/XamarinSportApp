df7f4c3961cab749d6c74550852099893a6bb869
Vikram <vickeyram@gmail.com>
2018-02-12 16:18:24 +0530

    Web and webapi version also committed

---
+++ Web/Fanword/Fanword.Data/IdentityConfig/Enums/AppRoles.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fanword.Data.Enums {
+    public static class AppRoles {
+        public const string QualifierSystem = "System";
+        public const string SuffixAdmin = "Admin";
+        public static string SystemAdmin = AppRules.Join(QualifierSystem, SuffixAdmin);
+
+        public static string ContentSource = "ContentSource";
+        public static string TeamAdmin = "TeamAdmin";
+        public static string SchoolAdmin = "SchoolAdmin";
+        public static string Advertiser = "Advertiser";
+    }
+}
